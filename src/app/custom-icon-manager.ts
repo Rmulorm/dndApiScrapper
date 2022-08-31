4d65bbd056bbb8098498986a0766f72bdb15240a
Romulo Maciel <romulorennerm@yahoo.com.br>
2022-08-31 17:03:01 -0300

    refactor: moved all custon icon creation to a Util class

---
+++ src/app/custom-icon-manager.ts
@@ -0,0 +1,87 @@
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+import { DND_RACES_ICON, DND_SPELL_ICON } from "./assets/categories-icons";
+import { DND_LOGO_ICON } from "./assets/core-icons";
+import {
+  ABJURATION_ICON,
+  CONJURATION_ICON,
+  DIVINATION_ICON,
+  ENCHANTMENT_ICON,
+  EVOCATION_ICON,
+  ILLUSION_ICON,
+  NECROMANCY_ICON,
+  TRANSMUTATION_ICON,
+} from "./assets/magic-schools-icons";
+
+export default class CustomIconManager {
+  public static setCustomIcons(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ): void {
+    this.setCoreIcons(iconRegistry, sanitizer);
+    this.setCategoriesIcons(iconRegistry, sanitizer);
+    this.setSpellSchoolsIcons(iconRegistry, sanitizer);
+  }
+
+  private static setCoreIcons(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIconLiteral(
+      "dnd-logo-svg",
+      sanitizer.bypassSecurityTrustHtml(DND_LOGO_ICON)
+    );
+  }
+
+  private static setCategoriesIcons(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIconLiteral(
+      "dnd-races-svg",
+      sanitizer.bypassSecurityTrustHtml(DND_RACES_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "dnd-spells-svg",
+      sanitizer.bypassSecurityTrustHtml(DND_SPELL_ICON)
+    );
+  }
+
+  private static setSpellSchoolsIcons(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIconLiteral(
+      "abjuration-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(ABJURATION_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "conjuration-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(CONJURATION_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "divination-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(DIVINATION_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "enchantment-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(ENCHANTMENT_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "evocation-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(EVOCATION_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "illusion-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(ILLUSION_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "necromancy-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(NECROMANCY_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      "transmutation-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(TRANSMUTATION_ICON)
+    );
+  }
+}
--- src/app/spells-list/spells-list.component.ts
+++ src/app/spells-list/spells-list.component.ts
@@ -1,17 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
-import { MatIconRegistry } from "@angular/material/icon";
 import { Apollo, gql } from "apollo-angular";
-import {
-  ABJURATION_ICON,
-  CONJURATION_ICON,
-  DIVINATION_ICON,
-  ENCHANTMENT_ICON,
-  EVOCATION_ICON,
-  ILLUSION_ICON,
-  NECROMANCY_ICON,
-  TRANSMUTATION_ICON,
-} from "../assets/magic-schools-icons";
 
 type School = {
   index: string;
@@ -40,44 +28,7 @@ export class SpellsListComponent implements OnInit {
   loading = true;
   error: any;
 
-  constructor(
-    private apollo: Apollo,
-    iconRegistry: MatIconRegistry,
-    sanitizer: DomSanitizer
-  ) {
-    iconRegistry.addSvgIconLiteral(
-      "abjuration-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(ABJURATION_ICON)
-    );
-    iconRegistry.addSvgIconLiteral(
-      "conjuration-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(CONJURATION_ICON)
-    );
-    iconRegistry.addSvgIconLiteral(
-      "divination-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(DIVINATION_ICON)
-    );
-    iconRegistry.addSvgIconLiteral(
-      "enchantment-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(ENCHANTMENT_ICON)
-    );
-    iconRegistry.addSvgIconLiteral(
-      "evocation-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(EVOCATION_ICON)
-    );
-    iconRegistry.addSvgIconLiteral(
-      "illusion-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(ILLUSION_ICON)
-    );
-    iconRegistry.addSvgIconLiteral(
-      "necromancy-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(NECROMANCY_ICON)
-    );
-    iconRegistry.addSvgIconLiteral(
-      "transmutation-icon-svg",
-      sanitizer.bypassSecurityTrustHtml(TRANSMUTATION_ICON)
-    );
-  }
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
     this.apollo
