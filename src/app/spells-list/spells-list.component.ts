7da6a4fcce48fa1a76cad1f3469abe79df549016
Romulo Maciel <romulorennerm@yahoo.com.br>
2022-08-31 15:28:06 -0300

    feat: implement proper school icons for spells

--- src/app/spells-list/spells-list.component.ts
+++ src/app/spells-list/spells-list.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material/icon';
-import { Apollo, gql } from 'apollo-angular';
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry } from "@angular/material/icon";
+import { Apollo, gql } from "apollo-angular";
 import {
   ABJURATION_ICON,
   CONJURATION_ICON,
   DIVINATION_ICON,
   ENCHANTMENT_ICON,
   EVOCATION_ICON,
-  ILUSION_ICON,
+  ILLUSION_ICON,
   NECROMANCY_ICON,
   TRANSMUTATION_ICON,
-} from '../assets/magic-schools-icons';
+} from "../assets/magic-schools-icons";
 
 type School = {
   index: string;
@@ -31,9 +31,9 @@ type Response = {
 };
 
 @Component({
-  selector: 'app-spells-list',
-  templateUrl: './spells-list.component.html',
-  styleUrls: ['./spells-list.component.scss'],
+  selector: "app-spells-list",
+  templateUrl: "./spells-list.component.html",
+  styleUrls: ["./spells-list.component.scss"],
 })
 export class SpellsListComponent implements OnInit {
   spells: Spell[] = [];
@@ -46,35 +46,35 @@ export class SpellsListComponent implements OnInit {
     sanitizer: DomSanitizer
   ) {
     iconRegistry.addSvgIconLiteral(
-      'abjuration-icon-svg',
+      "abjuration-icon-svg",
       sanitizer.bypassSecurityTrustHtml(ABJURATION_ICON)
     );
     iconRegistry.addSvgIconLiteral(
-      'conjuration-icon-svg',
+      "conjuration-icon-svg",
       sanitizer.bypassSecurityTrustHtml(CONJURATION_ICON)
     );
     iconRegistry.addSvgIconLiteral(
-      'divination-icon-svg',
+      "divination-icon-svg",
       sanitizer.bypassSecurityTrustHtml(DIVINATION_ICON)
     );
     iconRegistry.addSvgIconLiteral(
-      'enchantment-icon-svg',
+      "enchantment-icon-svg",
       sanitizer.bypassSecurityTrustHtml(ENCHANTMENT_ICON)
     );
     iconRegistry.addSvgIconLiteral(
-      'evocation-icon-svg',
+      "evocation-icon-svg",
       sanitizer.bypassSecurityTrustHtml(EVOCATION_ICON)
     );
     iconRegistry.addSvgIconLiteral(
-      'ilusion-icon-svg',
-      sanitizer.bypassSecurityTrustHtml(ILUSION_ICON)
+      "illusion-icon-svg",
+      sanitizer.bypassSecurityTrustHtml(ILLUSION_ICON)
     );
     iconRegistry.addSvgIconLiteral(
-      'necromancy-icon-svg',
+      "necromancy-icon-svg",
       sanitizer.bypassSecurityTrustHtml(NECROMANCY_ICON)
     );
     iconRegistry.addSvgIconLiteral(
-      'transmutation-icon-svg',
+      "transmutation-icon-svg",
       sanitizer.bypassSecurityTrustHtml(TRANSMUTATION_ICON)
     );
   }
@@ -82,24 +82,28 @@ export class SpellsListComponent implements OnInit {
   ngOnInit() {
     this.apollo
       .watchQuery({
-        query: gql<Response, any>`query GetSpells {
-          spells {
-            index
-            name
-            level
-            school {
+        query: gql<Response, any>`
+          query GetSpells {
+            spells {
               index
               name
+              level
+              school {
+                index
+                name
+              }
             }
           }
-        }`,
+        `,
       })
       .valueChanges.subscribe((result) => {
-        this.spells = result?.data?.spells;
         this.loading = result.loading;
         this.error = result.error;
-        this.spells.forEach((spell) => {
-          spell.schoolIcon = this.getSchoolIcon(spell.school.name);
+        result?.data?.spells.forEach((spell) => {
+          this.spells.push({
+            ...spell,
+            schoolIcon: this.getSchoolIcon(spell.school.index),
+          });
         });
       });
   }
