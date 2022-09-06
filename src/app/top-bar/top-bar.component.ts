fc94cbae203defa0eded8fce7ce90a5ccca7ee64
Romulo Maciel <romulorennerm@yahoo.com.br>
2022-09-05 22:51:20 -0300

    refactor: enhanve top bar

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
 import { MatIconRegistry } from "@angular/material/icon";
 import CustomIconManager from "../custom-icon-manager";
+import { Location } from "@angular/common";
+import { Router, RouterEvent } from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
   selector: "app-top-bar",
@@ -9,9 +12,27 @@ import CustomIconManager from "../custom-icon-manager";
   styleUrls: ["./top-bar.component.scss"],
 })
 export class TopBarComponent implements OnInit {
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  isButtonDisabled: boolean;
+
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    router: Router,
+    private location: Location
+  ) {
     CustomIconManager.setCustomIcons(iconRegistry, sanitizer);
+    router.events.subscribe(() => {
+      this.isButtonDisabled = this.location.isCurrentPathEqualTo("");
+    });
   }
 
   ngOnInit(): void {}
+
+  backButton() {
+    this.location.back();
+  }
+
+  dndButtonClick() {
+    window.location.href = "https://dnd.wizards.com/";
+  }
 }
